68c5bd765258f5d8a5e8afb3b0b3acb65dfac7e2
Steve Michelotti <steve.michelotti@gmail.com>
2015-05-14 09:52:07 -0400

    added tracing messages

--- MVPVConfDemo/Controllers/HomeController.cs
+++ MVPVConfDemo/Controllers/HomeController.cs
@@ -11,6 +11,9 @@ namespace MVPVConfDemo.Controllers
     {
         public ActionResult Index()
         {
+            Trace.TraceInformation("This is my information message.");
+            Trace.TraceWarning("This is cool *warning* message.");
+
             return View();
         }
 
